f5e52e0af9b4ffa57446050c2c20654faf7c305c
ashley.morris <ashleym9000@googlemail.com>
2021-07-12 11:34:17 +0100

    Added DB migrations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Net.NetworkInformation;
+using CF247TechTest.API.Data.Contexts;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CF247TechTest.API
 {
@@ -8,6 +12,14 @@ namespace CF247TechTest.API
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<CustomerContext>();
+                context.Database.Migrate();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
